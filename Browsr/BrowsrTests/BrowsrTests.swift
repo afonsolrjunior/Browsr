2bcc2071123c074bd18d742660ab2094b288bd30
Afonso Rodrigues <afonso.rodrigues@zurich.ch>
2023-02-12 19:55:56 +0000

    Created the base structure for the app

---
+++ Browsr/BrowsrTests/BrowsrTests.swift
@@ -0,0 +1,13 @@
+//
+//  BrowsrTests.swift
+//  BrowsrTests
+//
+//  Created by Afonso Rodrigues (Contractor) on 12/02/2023.
+//
+
+import XCTest
+@testable import Browsr
+
+final class BrowsrTests: XCTestCase {
+
+}
---
+++ Browsr/BrowsrUITests/BrowsrUITestsLaunchTests.swift
@@ -0,0 +1,12 @@
+//
+//  BrowsrUITestsLaunchTests.swift
+//  BrowsrUITests
+//
+//  Created by Afonso Rodrigues (Contractor) on 12/02/2023.
+//
+
+import XCTest
+
+final class BrowsrUITestsLaunchTests: XCTestCase {
+
+}
